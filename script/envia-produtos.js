bc095bf843d17b008a697ee19101615e6691ebd5
MeSGuel <miguelmgsantos64@gmail.com>
2024-06-22 18:44:21 -0300

    dsadas

--- script/envia-produtos.js
+++ script/envia-produtos.js
@@ -5,7 +5,6 @@ form.addEventListener('submit', e => enviaProdutos(e));
 
 async function enviaProdutos(e) {
     e.preventDefault();
-    e.stopPropagation();
 
     let nome = document.getElementById('nome').value;
     let preco = document.getElementById('preco').value;
@@ -13,11 +12,7 @@ async function enviaProdutos(e) {
 
     try {
         await criaProdutos(nome, preco, imagem);
-        const form = e.target;
-        form.submit();
     } catch (erro) {
         console.error('Erro ao enviar o produto:', erro);
     }
-
-    return false;
 }
